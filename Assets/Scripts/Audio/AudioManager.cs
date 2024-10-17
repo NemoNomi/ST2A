4a31c61f0eaa4af91f2a91e67d357f6e16a5a392
NemoNomi <162903221+NemoNomi@users.noreply.github.com>
2024-10-17 22:27:02 +0200

    added Audio Mixer, AudioManager, VolumeManager, PauseMenu with Settings and Audio Settings

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Source")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+    [SerializeField] AudioSource UISource;
+    [SerializeField] private VolumeSettings volumeSettings;
+
+    [Header("Audio Mixer")]
+    public AudioMixer audioMixer;
+    
+    [Header("Audio Clip")]
+    public AudioClip background;
+    public AudioClip PauseMenuOpen;
+    public AudioClip PauseMenuClose;
+    public AudioClip UISelect;
+    public AudioClip UISubmit;
+
+    #region Singleton
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+
+    public void PlayUI(AudioClip clip)
+    {
+        UISource.PlayOneShot(clip);
+    }
+
+    public void SetLowpassFilter(float frequency)
+    {
+        audioMixer.SetFloat("LowpassFreq", frequency);
+    }
+}
---
+++ Assets/Scripts/Menus/PauseMenuManager.cs
@@ -0,0 +1,160 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    [Header("Pause Menu Canvas")]
+    [SerializeField] private GameObject pauseMenuCanvasGO;
+    [SerializeField] private GameObject pauseMenuPanel;
+    [SerializeField] private GameObject settingsMenuPanel;
+    [SerializeField] private GameObject audioMenuPanel;
+
+    [Header("First Selected Options")]
+    [SerializeField] private GameObject pauseMenuFirstSelected;
+    [SerializeField] private GameObject settingsMenuFirstSelected;
+    [SerializeField] private GameObject audioMenuFirstSelected;
+
+    [Header("Pause Button")]
+    [SerializeField] private Button pauseButton;
+
+    private bool isPaused = false;
+
+    private void Start()
+    {
+        InitializeMenus();
+
+        if (pauseButton != null)
+        {
+            pauseButton.onClick.AddListener(() => TogglePauseMenu());
+        }
+    }
+
+    private void Update()
+    {
+        HandlePauseInput();
+    }
+
+    private void InitializeMenus()
+    {
+        pauseMenuCanvasGO.SetActive(false);
+        pauseMenuPanel.SetActive(false);
+        settingsMenuPanel.SetActive(false);
+        audioMenuPanel.SetActive(false);
+    }
+
+    private void HandlePauseInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseMenu();
+        }
+    }
+
+    private void TogglePauseMenu()
+    {
+        if (!isPaused)
+        {
+            PauseGame();
+        }
+        else
+        {
+            ResumeGame();
+        }
+    }
+
+    private void PauseGame()
+    {
+        isPaused = true;
+        Time.timeScale = 0f;
+        OpenPauseMenu();
+        AudioManager.instance.PlayUI(AudioManager.instance.PauseMenuOpen);
+        if (!audioMenuPanel.activeInHierarchy)
+        {
+            AudioManager.instance.SetLowpassFilter(500f);
+        }
+    }
+
+    private void ResumeGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+        CloseAllMenus();
+        AudioManager.instance.PlayUI(AudioManager.instance.PauseMenuClose);
+        AudioManager.instance.SetLowpassFilter(22000f);
+    }
+
+    private void SetMenuVisibility(bool isVisible)
+    {
+        pauseMenuCanvasGO.SetActive(isVisible);
+    }
+
+    private void OpenPauseMenu()
+    {
+        SetMenuVisibility(true);
+        pauseMenuPanel.SetActive(true);
+        settingsMenuPanel.SetActive(false);
+        audioMenuPanel.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(pauseMenuFirstSelected);
+    }
+
+    private void OpenSettingsMenu()
+    {
+        pauseMenuPanel.SetActive(false);
+        settingsMenuPanel.SetActive(true);
+        audioMenuPanel.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(settingsMenuFirstSelected);
+    }
+
+    private void OpenAudioMenu()
+    {
+        settingsMenuPanel.SetActive(false);
+        audioMenuPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(audioMenuFirstSelected);
+        AudioManager.instance.SetLowpassFilter(22000f);
+    }
+
+    private void CloseAllMenus()
+    {
+        SetMenuVisibility(false);
+        pauseMenuPanel.SetActive(false);
+        settingsMenuPanel.SetActive(false);
+        audioMenuPanel.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void OnResumePress()
+    {
+        ResumeGame();
+    }
+
+    public void OnMainMenuPress()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnSettingsPress()
+    {
+        OpenSettingsMenu();
+    }
+
+    public void OnAudioPress()
+    {
+        OpenAudioMenu();
+    }
+
+    public void OnSettingsBackPress()
+    {
+        settingsMenuPanel.SetActive(false);
+        OpenPauseMenu();
+    }
+
+    public void OnAudioBackPress()
+    {
+        audioMenuPanel.SetActive(false);
+        OpenSettingsMenu();
+        AudioManager.instance.SetLowpassFilter(500f);
+    }
+}
