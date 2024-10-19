5de0f310fe8af183f5d15f175d184af8fd5824b7
NemoNomi <162903221+NemoNomi@users.noreply.github.com>
2024-10-19 10:53:36 +0200

    added physics collision with MiddleLine

--- Assets/Scripts/Camera/CameraBoundaries.cs
+++ Assets/Scripts/Camera/CameraBoundaries.cs
@@ -17,11 +17,11 @@ public class CameraBoundaries : MonoBehaviour
     public GameObject middleLinePlayer1;
     public GameObject middleLinePlayer2;
 
+    private BoxCollider2D[] middleLineCollidersPlayer1;
+    private BoxCollider2D[] middleLineCollidersPlayer2;
+
     private float minXLeft, maxXLeft, minYLeft, maxYLeft;
     private float minXRight, maxXRight, minYRight, maxYRight;
-
-    private BoxCollider2D middleLineColliderPlayer1;
-    private BoxCollider2D middleLineColliderPlayer2;
 
     void Start()
     {
@@ -42,32 +42,34 @@ public class CameraBoundaries : MonoBehaviour
 
         if (middleLinePlayer1 != null)
         {
-            middleLineColliderPlayer1 = middleLinePlayer1.GetComponent<BoxCollider2D>();
-            if (middleLineColliderPlayer1 == null)
+            middleLineCollidersPlayer1 = middleLinePlayer1.GetComponentsInChildren<BoxCollider2D>();
+            if (middleLineCollidersPlayer1.Length == 0)
             {
-                Debug.LogError("Player 1 middle line does not have a BoxCollider2D component.");
+                Debug.LogError("Player 1 middle line has no BoxCollider2D components.");
             }
         }
 
         if (middleLinePlayer2 != null)
         {
-            middleLineColliderPlayer2 = middleLinePlayer2.GetComponent<BoxCollider2D>();
-            if (middleLineColliderPlayer2 == null)
+            middleLineCollidersPlayer2 = middleLinePlayer2.GetComponentsInChildren<BoxCollider2D>();
+            if (middleLineCollidersPlayer2.Length == 0)
             {
-                Debug.LogError("Player 2 middle line does not have a BoxCollider2D component.");
+                Debug.LogError("Player 2 middle line has no BoxCollider2D components.");
             }
         }
 
-        CalculateBounds();
+        CalculateCameraBounds();
     }
 
     void Update()
     {
-        CalculateBounds();
-        ClampPlayerPositions();
+        CalculateCameraBounds();
+        ClampPlayerPositionsCamera();
+        ClampPlayerPositionsCollisions();
     }
 
-    void CalculateBounds()
+    // --------- KAMERA BOUNDARIES ----------
+    void CalculateCameraBounds()
     {
         if (pixelPerfectCamera == null)
             return;
@@ -75,33 +77,24 @@ public class CameraBoundaries : MonoBehaviour
         Vector3 bottomLeft = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, mainCamera.nearClipPlane));
         Vector3 topRight = mainCamera.ViewportToWorldPoint(new Vector3(1, 1, mainCamera.nearClipPlane));
 
-        float middleLineXPlayer1 = middleLinePlayer1.transform.position.x;
-        float middleLineXPlayer2 = middleLinePlayer2.transform.position.x;
-
         minXLeft = bottomLeft.x + buffer;
-        maxXLeft = middleLineXPlayer1 - buffer;
+        maxXLeft = topRight.x / 2f - buffer;
         minYLeft = bottomLeft.y + buffer;
         maxYLeft = topRight.y - buffer;
 
-        minXRight = middleLineXPlayer2 + buffer;
+        minXRight = topRight.x / 2f + buffer;
         maxXRight = topRight.x - buffer;
         minYRight = bottomLeft.y + buffer;
         maxYRight = topRight.y - buffer;
     }
 
-    void ClampPlayerPositions()
+    void ClampPlayerPositionsCamera()
     {
         if (player1 != null)
         {
             Vector3 pos1 = player1.position;
             pos1.x = Mathf.Clamp(pos1.x, minXLeft, maxXLeft);
             pos1.y = Mathf.Clamp(pos1.y, minYLeft, maxYLeft);
-
-            if (middleLineColliderPlayer1 != null && player1.GetComponent<Collider2D>().IsTouching(middleLineColliderPlayer1))
-            {
-                pos1.x = Mathf.Min(pos1.x, middleLinePlayer1.transform.position.x - buffer);
-            }
-
             player1.position = pos1;
         }
 
@@ -110,10 +103,38 @@ public class CameraBoundaries : MonoBehaviour
             Vector3 pos2 = player2.position;
             pos2.x = Mathf.Clamp(pos2.x, minXRight, maxXRight);
             pos2.y = Mathf.Clamp(pos2.y, minYRight, maxYRight);
+            player2.position = pos2;
+        }
+    }
 
-            if (middleLineColliderPlayer2 != null && player2.GetComponent<Collider2D>().IsTouching(middleLineColliderPlayer2))
+    // --------- PHYSISCHE KOLLISION MIT MIDDLE LINE ----------
+    void ClampPlayerPositionsCollisions()
+    {
+        if (player1 != null)
+        {
+            Vector3 pos1 = player1.position;
+
+            foreach (var collider in middleLineCollidersPlayer1)
             {
-                pos2.x = Mathf.Max(pos2.x, middleLinePlayer2.transform.position.x + buffer);
+                if (collider != null && player1.GetComponent<Collider2D>().IsTouching(collider))
+                {
+                    pos1.x = Mathf.Min(pos1.x, collider.bounds.min.x - buffer);
+                }
+            }
+
+            player1.position = pos1;
+        }
+
+        if (player2 != null)
+        {
+            Vector3 pos2 = player2.position;
+
+            foreach (var collider in middleLineCollidersPlayer2)
+            {
+                if (collider != null && player2.GetComponent<Collider2D>().IsTouching(collider))
+                {
+                    pos2.x = Mathf.Max(pos2.x, collider.bounds.max.x + buffer);
+                }
             }
 
             player2.position = pos2;
